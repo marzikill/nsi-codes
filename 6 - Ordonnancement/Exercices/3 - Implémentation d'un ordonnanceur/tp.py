6c0810f842b931992100b1bfda2f6d840b72c5b2
Florian Picard <florian.picard.fr@gmail.com>
2024-12-16 15:00:44 +0100

    ordonnancement jusqua ordonnanceur fifo

--- 6 - Ordonnancement/Exercices/3 - Implémentation d'un ordonnanceur/tp.py
+++ 6 - Ordonnancement/Exercices/3 - Implémentation d'un ordonnanceur/tp.py
@@ -1,16 +1,21 @@
+from ds import File
+
 class Processus:
     """Représente un processus"""
     def __init__(self, pid, arrivee, duree):
-        """Processus, int, int, int -> Nonetype"""
-        pass
+        """Processus, int, int, int -> None"""
+        self.pid = pid
+        self.duree = duree
+        self.arrivee = arrivee
+        self.duree_restante = duree
 
     def __repr__(self):
         """ Processus -> str
         Renvoie une chaine de caractère représentant le processus self """
-        pass
+        return f"<P{self.pid}>"
     
     def avancer(self, d, t):
-        """ Processus, int, int -> Nonetype
+        """ Processus, int, int -> None
         Le processus (re)prend son exécution à la date t pour une durée d """
         pass
     
@@ -18,16 +23,7 @@ class Processus:
         """ Processus -> bool
         Détermine si le processus p a terminé son exécution """
         pass
-    
-    def __init__(self, pid, arrivee, duree):
-        """ Initialise un processus """
-        pass
-    
-    def avancer(self, d, t):
-        """ Processus, int, int -> Nonetype
-        Avance le processus """
-        pass
-    
+
     def sejour(self):
         """ Renvoie le temps de séjour de self """
         pass
@@ -47,16 +43,16 @@ class Processus:
 class Chronogramme:
     """Représente un chronogramme"""
     def __init__(self):
-        """Chronogramme -> Nonetype"""
-        pass
+        """Chronogramme -> None"""
+        self.table = []
 
     def temps_ecoule(self):
         """ Chronogramme -> int
         Renvoie le nombre de cycles écoulés depuis l'instant initial """
-        pass
+        return len(self.table)
     
     def enregistrer(self, p, q):
-        """ int, int -> Nonetype
+        """ int, int -> None
         Enregistre l'information : le processus p est exécuté pendant q cycles """
         pass
     
@@ -68,16 +64,18 @@ class Chronogramme:
 class OrdonnanceurFIFO:
     """Représente un ordonnanceur"""
     def __init__(self, q=1):
-        """OrdonnanceurFIFO, int -> Nonetype"""
-        pass
+        """OrdonnanceurFIFO, int -> None"""
+        self.chrono = Chronogramme()
+        self.file_processus = File()
+        self.quantum = q
 
     def ajoute_processus(self, p_list):
-        """ OrdonnanceurFIFO, [Processus] -> Nonetype
+        """ OrdonnanceurFIFO, [Processus] -> None
         Ajoute à self.file_processus les processus de p_list par ordre d'arrivée """
         pass
     
     def exec_quantum(self):
-        """ OrdonnanceurFIFO -> Nonetype
+        """ OrdonnanceurFIFO -> None
         Simule l'exécution d'un quantum """
         pass
     
@@ -90,4 +88,29 @@ def renvoie_et_supprime_premier(p_list):
     """ [Processus] -> Processus
     Renvoie et supprime de p_list le processus dont l'attribut arrivee est le plus petit """
     pass
+
+p1 = Processus(1, 10, 0)
+p2 = Processus(2, 4, 3)
+p3 = Processus(3, 8, 2)
+ordo = OrdonnanceurFIFO()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
