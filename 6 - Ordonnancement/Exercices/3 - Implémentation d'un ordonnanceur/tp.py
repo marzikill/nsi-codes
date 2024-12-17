166d3443bc29d3782642fe26e5eff17b70ff045e
Florian Picard <florian.picard.fr@gmail.com>
2024-12-17 16:05:56 +0100

    fin ordonnancement

--- 6 - Ordonnancement/Exercices/3 - Implémentation d'un ordonnanceur/tp.py
+++ 6 - Ordonnancement/Exercices/3 - Implémentation d'un ordonnanceur/tp.py
@@ -2,7 +2,7 @@ from ds import File
 
 class Processus:
     """Représente un processus"""
-    def __init__(self, pid, arrivee, duree):
+    def __init__(self, pid, duree, arrivee):
         """Processus, int, int, int -> None"""
         self.pid = pid
         self.duree = duree
@@ -17,12 +17,13 @@ class Processus:
     def avancer(self, d, t):
         """ Processus, int, int -> None
         Le processus (re)prend son exécution à la date t pour une durée d """
-        pass
-    
+        if self.arrivee <= t:
+            self.duree_restante -= d
+            
     def est_termine(self):
         """ Processus -> bool
         Détermine si le processus p a terminé son exécution """
-        pass
+        return self.duree_restante <= 0
 
     def sejour(self):
         """ Renvoie le temps de séjour de self """
@@ -54,12 +55,13 @@ class Chronogramme:
     def enregistrer(self, p, q):
         """ int, int -> None
         Enregistre l'information : le processus p est exécuté pendant q cycles """
-        pass
+        for i in range(q):
+            self.table.append(p)
     
     def __repr__(self):
         """ self -> str
         Renvoie une chaine de caractère représentant le chronogramme self """
-        pass
+        return str(self.table)
 
 class OrdonnanceurFIFO:
     """Représente un ordonnanceur"""
@@ -72,30 +74,56 @@ class OrdonnanceurFIFO:
     def ajoute_processus(self, p_list):
         """ OrdonnanceurFIFO, [Processus] -> None
         Ajoute à self.file_processus les processus de p_list par ordre d'arrivée """
-        pass
+        for i in range(len(p_list)):
+            p = renvoie_et_supprime_premier(p_list)
+            self.file_processus.enfiler(p)
     
     def exec_quantum(self):
         """ OrdonnanceurFIFO -> None
         Simule l'exécution d'un quantum """
-        pass
-    
+        t = self.chrono.temps_ecoule()
+        p = self.file_processus.examine()
+        self.chrono.enregistrer(p, self.quantum)
+        p.avancer(self.quantum, t)
+        if p.est_termine():
+            self.file_processus.defiler()
+            
     def exec(self):
         """ OrdonnanceurFIFO -> Chronogramme
         Simule l'exécution des processus de la file et en renvoie le chronogramme """
-        pass
+        while not self.file_processus.est_vide():
+            self.exec_quantum()
 
 def renvoie_et_supprime_premier(p_list):
     """ [Processus] -> Processus
     Renvoie et supprime de p_list le processus dont l'attribut arrivee est le plus petit """
-    pass
+    mini = p_list[0].arrivee
+    i_mini = 0
+    for i in range(len(p_list)):
+        if p_list[i].arrivee < p_list[i_mini].arrivee:
+            i_mini = i
+    return p_list.pop(i_mini)
+
+# Il ne suffit pas de faire un parcours par valeur
+# car il faut supprimer le processus dont le temps
+# d'arrivée est le plus faible de p_list : pour ça
+# on a besoin de son indice.
+# def renvoie_et_supprime_premier(p_list):
+#     court_tempo = p_list[0]
+#     for process in p_list:
+#         if process.arrivee <= court_tempo.arrivee :
+#             court_tempo = process
+#     return court_tempo
 
 p1 = Processus(1, 10, 0)
 p2 = Processus(2, 4, 3)
 p3 = Processus(3, 8, 2)
+p_list = [p1, p2, p3]
+
 ordo = OrdonnanceurFIFO()
-
-
-
+ordo.ajoute_processus(p_list)
+print(ordo.file_processus)
+ordo.exec()
 
 
 
