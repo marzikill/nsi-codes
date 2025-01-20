09a4d94405cdf469754844a846919a504cf99393
Florian Picard <florian.picard.fr@gmail.com>
2025-01-20 13:13:21 +0100

    tournoi : début

---
+++ 7 - Arbres binaires/3 - Tournoi/tp.py
@@ -0,0 +1,55 @@
+def nb_joueurs(a):
+    """ Arbre -> int
+    Renvoie le nombre de joueurs ayant participé à la compétition d'arbre a """
+    pass
+
+def nb_rounds(a):
+    """ Arbre -> int
+    Renvoie le nombre de rounds de la compétition a """
+    pass
+
+def occurrences(a, nom):
+    """ Arbre, str -> int
+    Renvoie le nombre d'occurrences de nom dans a """
+    pass
+
+def nombre_matchs(a, nom):
+    """ Arbre, str -> int
+    Renvoie le nombre de matchs joués par le joueur nom dans la compétition d'arbre a """
+    pass
+
+def liste_joueurs(a):
+    """ Arbre -> [str]
+    Renvoie la liste des joueurs ayant participé à la compétition d'arbre a """
+    # if est_vide(a):
+    #     return ....................................
+    # elif est_feuille(a):
+    #     return [..................................]
+    # else:
+    #     return ....................................
+
+def niveau(a, i):
+    """ Arbre, int -> [str]
+    Renvoie la liste des étiquettes des nœuds de profondeur i de l'arbre a """
+    pass
+
+def vaincus_gagnant(a):
+    """ Arbre -> [str]
+    Renvoie la liste des joueurs ayant joué un match contre le gagnant de la compétition d'arbre a """
+    pass
+
+def parcours_largeur(a):
+    """ Arbre -> [str]
+    Renvoie la liste des étiquettes des nœuds de l'arbre a, telle qu'obtenue à l'aide d'un parcours en largeur """
+    pass
+
+def recherche_noeud(a, nom):
+    """ Arbre, str -> Arbre
+    Renvoie le sous-arbre de a dont la racine a pour étiquette nom et est de profondeur minimale dans a """
+    pass
+
+def joueurs_vaincus(a, nom):
+    """ Arbre, str -> [str]
+    Renvoie la liste des joueurs vaincus par nom dans la compétition d'arbre a """
+    pass
+
