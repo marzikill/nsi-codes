f5f5f877fa465100f336d14229bea5b423f725a8
Florian Picard <florian.picard.fr@gmail.com>
2025-05-12 15:03:34 +0200

    parcours

--- 11 - Parcours de graphes/2 - Applications/tp.py
+++ 11 - Parcours de graphes/2 - Applications/tp.py
@@ -1,8 +1,52 @@
+from ds import *
 
 def existe_chemin(G, sommet, destination):
     """ Graphe, Sommet, Sommet -> bool
     Renvoie True si et seulement si il existe un chemin entre source et destination dans G. """
-    pass
+    l = parcours_largeur(G, sommet)
+    for i in range(len(l)):
+        if l[i] == destination:
+            return True
+    return False
+
+def existe_chemin_rec(G, depart, destination, vus):
+    """ Graphe, Sommet, Sommet, Ensemble -> bool
+    Parcourt le graphe G depuis depart en profondeur (sachant que
+    la liste des sommets présents dans vus ont déjà été visités). """
+    # Cas de base
+    if depart == destination:
+        return True
+    # Cas général
+    if depart not in vus:
+        vus.ajouter(depart)
+        # appels récursif sur les voisins 
+        for voisin in G.voisins(depart):
+            if existe_chemin_rec(G, voisin, destination, vus):
+                return True
+    return False
+
+def existe_chemin_file(G, depart, destination):
+    # À MODIFIER : IL FAUT S'ARRÊTER DÈS QU'ON
+    # A TROUVÉ UN CHEMIN
+    ordre = [] # on y ajoute les éléments
+    # dans l'ordre de parcours en largeur
+    vus = Ensemble()
+    à_explorer = File()
+    à_explorer.enfiler(depart)
+    while not à_explorer.est_vide():
+        sommet = à_explorer.defiler()
+        # vus est un ensemble : test
+        # d'appartenance en temps constant
+        if sommet not in vus: 
+            vus.ajouter(sommet) # marquer le sommet comme visité
+            ordre.append(sommet)
+            for destination in G.voisins(sommet):
+                à_explorer.enfiler(destination)
+    return ordre
+
+# Exemple d'application : est-ce que deux
+# ordinateurs connectés sur le graphe "internet"
+# peuvent communiquer entre eux ?
 
 def composantes_connexes(G):
     """ Graphe -> {Sommet:int}
@@ -19,7 +63,7 @@ def parcours_cycle(G, depart, couleurs):
     for destination in G.voisins(depart):
         if ...:
             return ...
-        elif ...
+        elif ...:
             return ...
     couleurs[...] = ...
     return ...
