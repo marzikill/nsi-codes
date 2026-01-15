b570a09e9784bf4fd08bfae1cff84965937981aa
marzikill <florian.picard.fr@gmail.com>
2026-01-15 08:00:11 +0100

    Update tp.py
    correction TP1

--- Chapitre 4/TP1/tp.py
+++ Chapitre 4/TP1/tp.py
@@ -79,7 +79,11 @@ def nombre_absences_indice(notes, i):
     Renvoie le nombre d'absences de l'élève d'indice i """
     # on compte le nombre de -1 dans la colonne
     # d'indice i du tableau
-    
+    nb = 0
+    for n in notes[i]:
+        if n == -1:
+            nb = nb + 1
+    return nb
 
 def nombre_absences(notes, eleve):
     """ [[str | int]], str -> int
@@ -87,6 +91,21 @@ def nombre_absences(notes, eleve):
     # on parcourt toutes les lignes
     # à la recherche de l'indice de la ligne dont le
     # premier élément est eleve
+    # On renvoie -1 si eleve n'est pas dans la liste
+    for i in range(nombre_eleves(notes)):
+        if notes[i][0] == eleve:
+            return nombre_absences_indice(notes, i)
+    return -1
+
+def est_representative(notes, i):
+    nb_abs_maxi = int(1/3*nombre_notes(notes)) + 1
+    return nombre_absences_indice(notes, i) < nb_abs_maxi
+
+def convocation_rattrapage(notes):
+    """ [[ str | int ]] -> [str]
+    Renvoie la liste des élèves à convoquer au rattrapage """
+    return [notes[i][0] for i in range(nombre_eleves(notes))
+            if not est_representative(notes, i)]
 
 def moyenne_devoir(notes, n):
     """ [[str | int]], int -> int
@@ -96,11 +115,50 @@ def moyenne_devoir(notes, n):
     # on additionne toutes les notes des élèves
     # présents au devoir n et on compte le nombre
     # d'élèves présents.
+    s = 0
+    nb_presents = 0
+    for i in range(nombre_eleves(notes)):
+        if notes[i][n] != -1:
+            s = s + notes[i][n]
+            nb_presents = nb_presents + 1
+    return round(s/nb_presents, 2)
 
 def pire_devoir(notes):
     """ [[str | int]] -> int
     Renvoie le numéro du devoir dont la moyenne est la plus faible
     """
-    pass
+        if nombre_notes(notes) == 0:
+        return -1
+    n_mini = 1
+    pire = moyenne_devoir(notes, 1)
+    for n in range(1, nombre_notes(notes) + 1):
+        if moyenne_devoir(notes, n) < pire:
+            n_mini = n
+            pire = moyenne_devoir(notes, n)
+    return n_mini      
+
+def moyennes(notes):
+    """ [[str | int]] -> int
+    Renvoie la moyenne des élèves de la classe """
+    # on initialise le tableau des moyennes : il s'agit d'une liste 
+    # dont tous les éléments sont des listes du type 
+    # [prénom de l'élève, -1]
+    # notes[i][0] correspond au prénom de l'élève d'indice i
+    m = [[notes[i][0], -1] for i in range(nombre_eleves(notes))]
+    joker = pire_devoir(notes)
+    for i in range(nombre_eleves(notes)):
+        nb_notes = 0
+        s = 0
+        for n in range(1, nombre_notes(notes) + 1):
+            # On compte le devoir si l'élève est présent à
+            # celui-ci et si ça n'est pas le pire devoir
+            if notes[i][n] != -1 and n != joker:
+                nb_notes += 1
+                s += notes[i][n]
+        # On ne compte les notes que si la moyenne est
+        # représentative 
+        if est_representative(notes, i):
+            m[i][1] = round(s/nb_notes, 2)
+    return m
     
 
