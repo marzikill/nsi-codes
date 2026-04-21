d67db44dc026bbce07ed6bd74480853b9d6497f1
Florian PICARD <florian.picard@fustel.local>
2026-04-21 11:45:02 +0100

    dico

--- Chapitre 6 - Dictionnaires/TP2/tp2.py
+++ Chapitre 6 - Dictionnaires/TP2/tp2.py
@@ -52,3 +52,41 @@ def top_likes_mieux(likes):
         elif likes[prenom] == maxi:
             noms.append(prenom)
     return noms
+
+
+votes = ["Ali", "Bob", "Mél", "Ali", "Ali", "Mél"]
+
+def compte_voix(votes):
+    """ [str] -> {str: int}
+    Renvoie le decompte des voix des votes """
+    decompte = {}
+    for nom in votes:
+        if nom not in decompte:
+            decompte[nom] = 1
+        else:
+            decompte[nom] = decompte[nom] + 1
+    return decompte
+    
+    
+def pourcentage(decompte, nom):
+    """ {str: int}, str -> float
+    Renvoie le pourcentage de votes obtenu par nom dans décompte """
+    # À compléter
+    
+def resultat_second_tour(decompte):
+    """ {str: int} -> str
+    Renvoie le nom de la personne ayant obtenu le plus de votes """
+    decompte[''] = 0
+    meilleur = ''
+    # À compléter
+    
+def deux_meilleurs(decompte):
+    """ {str: int} -> {str: str}
+    Renvoie un dictionnaire correspondant aux deux vote  majoritaires """
+    decompte[''] = 0
+    podium = {'1er': '', '2nd': ''}
+    # À compléter
+    
+def resultat_premier_tour(decompte):
+    """ {str: int} -> [str]
+    Renvoie la ou les personnes gagnantes au premier tour """
